36cb3643b303e2b08c20c35b986044d9216a286e
MarcoStammComunica <marco.stammm@gmail.com>
2025-06-07 19:37:37 -0400

    Delete app/404.tsx

--- app/404.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Página não encontrada</h1>
-      <p>Verifique se o endereço está correto.</p>
-    </div>
-  );
-}
